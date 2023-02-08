ed67fe4fd48824f9b0b04a731d57d4517eb7c473
Second Son <118853502+ElManuer@users.noreply.github.com>
2023-02-07 20:58:05 -0400

    Add files via upload

---
+++ Cambio de moneda.py
@@ -0,0 +1,27 @@
+print()
+monedas = {"Euro":"€","Dollar":"$","Yen":"¥"}
+print(monedas.keys())
+
+print()
+buscar = input("Ingresa la moneda a buscar :").title
+print(monedas.get(buscar(),"Moneda Inexistente"))
+
+print()
+for i in range(3):
+    agregar = input("\nIngrese el nombre de la moneda a registrar :")
+    print(f"\nSu moneda '{agregar}' esta en la lista")
+    simbolo = input("\nIngrese el simbolo de su moneda registrada :")
+    monedas[agregar] = simbolo
+    print(monedas)
+
+print()
+eliminar = input("Ingrese la moneda a eliminar :")
+del monedas[eliminar]
+
+print()
+print(f"Usted elimino '{eliminar}' de la lista de monedas")
+print(f"\nEstan son sus monedas {monedas}")
+
+print()
+
+'''ACTIVIDAD REALIZADA POR MANUEL ENRIQUE PASTRANO SUAREZ ☂️'''
