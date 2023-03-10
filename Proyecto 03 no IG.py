643b49e4bf961e178dc4be66e8c9cfee67894dc1
Second Son <118853502+ElManuer@users.noreply.github.com>
2023-03-09 23:33:51 -0400

    Update Proyecto 03 no IG.py

--- Proyecto 03 no IG.py
+++ Proyecto 03 no IG.py
@@ -1,44 +1,44 @@
 import requests
+while True:
+  '''API PARA PEDIR LOS SIMBOLOS'''
+  url = "https://api.apilayer.com/fixer/symbols"
 
-'''API PARA PEDIR LOS SIMBOLOS'''
-url = "https://api.apilayer.com/fixer/symbols"
+  payload = {}
+  headers= {
+    "apikey": "s50VQQ5EDm0PoVBekdYzQAusXQn40lmz"
+  }
 
-payload = {}
-headers= {
-  "apikey": "s50VQQ5EDm0PoVBekdYzQAusXQn40lmz"
-}
+  r1 = requests.request("GET", url, headers=headers, data = payload)
+  j = r1.json()
+  listado = ['USD','VES','JPY','EUR','BTC']
 
-r1 = requests.request("GET", url, headers=headers, data = payload)
-j = r1.json()
-listado = ['USD','VES','JPY','EUR','BTC']
+  print('''
+        Bienvenido a conversor API
+  Por favor escoja la moneda que quiere convertir''')
+  for x in range(len(listado)):
+      print(listado[x])
 
-print('''
-      Bienvenido a conversor API
-Por favor escoja la moneda que quiere convertir''')
-for x in range(len(listado)):
-    print(listado[x])
+  fromm = input("\nIngrese la moneda que desee convertir: ").upper()
+  cantidad = int(input(f"\nIngrese la cantidad de '{fromm}' a convertir: "))
 
-fromm = input("\nIngrese la moneda que desee convertir: ").upper()
-cantidad = int(input(f"\nIngrese la cantidad de '{fromm}' a convertir: "))
+  print()
+  for x in range(len(listado)):
+      print(listado[x])
+  to = input("\nIngrese la moneda a la que la convertira: ").upper()
 
-print()
-for x in range(len(listado)):
-    print(listado[x])
-to = input("\nIngrese la moneda a la que la convertira: ").upper()
+  '''API PARA HACER LOS CAMBIOS'''
+  url = f"https://api.apilayer.com/fixer/convert?to={to}&from={fromm}&amount={cantidad}"
 
-'''API PARA HACER LOS CAMBIOS'''
-url = f"https://api.apilayer.com/fixer/convert?to={to}&from={fromm}&amount={cantidad}"
+  payload = {}
+  headers= {
+    "apikey": "s50VQQ5EDm0PoVBekdYzQAusXQn40lmz"
+  }
 
-payload = {}
-headers= {
-  "apikey": "s50VQQ5EDm0PoVBekdYzQAusXQn40lmz"
-}
-
-r = requests.request("GET", url, headers=headers, data = payload)
-j = r.json()
+  r = requests.request("GET", url, headers=headers, data = payload)
+  j = r.json()
 
 
-total = round(j.get('result'),2)
-print(f"\n\tLa cantidad de '{fromm}' a '{to}' es de {total}")
+  total = round(j.get('result'),2)
+  print(f"\n\tLa cantidad de '{fromm}' a '{to}' es de {total}")
 
-'''ACTIVIDAD REALIZADA POR MANUEL ☂️'''
+  '''ACTIVIDAD REALIZADA POR MANUEL ☂️'''
